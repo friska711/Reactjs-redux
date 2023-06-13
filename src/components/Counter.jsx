20dab22f383bee4161f4a31226a41066fd164bef
friska711 <126258596+friska711@users.noreply.github.com>
2023-06-13 19:58:27 +0700

    Add files via upload

---
+++ src/components/Counter.jsx
@@ -0,0 +1,17 @@
+import  {useDispatch, useSelector} from "react-redux";
+import { increment } from "../store/counter";
+export const Counter = (props) => {
+    const count = useSelector (state => state.counter.value)
+    const user = useSelector( state => state.user)
+    const dispatch = useDispatch();
+    return <div>
+        <h2>Counter: {props.name}</h2>
+        <h2>User: {user.name}</h2>
+        <h2>Email: {user.email}</h2>
+        <h2>Street Address: {user.address.street}</h2>
+        <p>Count: {count}</p>
+        <button onClick={ () => dispatch(increment())}>
+            Increment
+        </button>
+    </div>
+}
